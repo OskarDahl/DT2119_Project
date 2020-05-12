63e29c4a36310069558c5aa41fd3fe39212828ec
Jakob Vyth <vyth@kth.se>
2020-05-12 10:03:05 +0200

    Add load_data function for loading data from testset.npz and trainset.npz

---
+++ tools.py
@@ -0,0 +1,33 @@
+import numpy as np
+def load_data(file_path, one_hot=False):
+    '''
+    Input
+        filepath: path to a .npz file
+        one_hot: if true, targets are output as one-hot vectors
+    Output
+        mfccs: 13x190 mfcc maps
+        targets: 0-9 genre target (or one-hot vectors)
+    '''
+    data = np.load(file_path, allow_pickle=True)
+    key = list(data.keys())[0]
+    data = data[key]
+    mfccs = []
+    targets = []
+    for item in data:
+        target = item['target']
+        mfcc = item['mfcc']
+        n_frames = mfcc.shape[1]
+        idx = 0
+        while(idx+190 < n_frames):
+            targets.append(target)
+            mfccs.append(mfcc[:,idx:idx+190])
+            idx += 190
+    if one_hot:
+        oh_targets = np.zeros((len(targets), max(targets)+1))
+        oh_targets[np.arange(len(targets)),targets] = 1
+        targets = oh_targets
+    else:
+        targets = np.array(targets)
+    return np.stack(mfccs), targets
+
+m.shape,t.shape
